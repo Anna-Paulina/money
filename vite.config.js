7c971e86a1a08b4a3dffedbb441e0fe34941a8d2
Anna-Paulina <134393584+Anna-Paulina@users.noreply.github.com>
2026-03-25 08:44:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// Change '/stock-tracker/' to match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/stock-tracker/',
+  base: '/money/',
 })
